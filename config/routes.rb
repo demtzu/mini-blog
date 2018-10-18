dc888120bb2db07689abf9184c8b324d649f466d
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-10-18 15:06:01 +0200

    list of articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'articles' => "articles#index"
+  get 'articles/:id' => 'articles#show',  as: :article
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
