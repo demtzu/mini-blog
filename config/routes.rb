41848636356f3c5816f95d237c3a598052e35476
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-10-18 15:24:05 +0200

    new route for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'articles' => "articles#index"
   get 'articles/:id' => 'articles#show',  as: :article
-  root to: 'pages#home'
+  root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
